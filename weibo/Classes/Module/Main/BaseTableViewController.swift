98da25cffd4e0fac109e2cc5a885fdfb24dc8a63
lanqiyuer <641104774@qq.com>
2015-11-27 21:59:36 -0800

    访问视图的初步实现

--- weibo/Classes/Module/Main/BaseTableViewController.swift
+++ weibo/Classes/Module/Main/BaseTableViewController.swift
@@ -1,5 +1,5 @@
 //
-//  HomeTableViewController.swift
+//  BaseTableViewController.swift
 //  weibo
 //
 //  Created by mac on 15/11/27.
@@ -8,8 +8,59 @@
 
 import UIKit
 
-class HomeTableViewController: BaseTableViewController {
+class BaseTableViewController: UITableViewController {
 
+    //添加一个用户登录标记
+    var userLogin = true
+    
+    
+    
+    /*
+    loadView() 准备视图层次结构加载视图，是苹果公司为手写代码专门准备的方法，
+    因为一旦实现loadView()，sb/xib会自动失效
+    loadView()方法会自动检测视图是否为空，如果为空会自动调用loadView()来加载视图
+    所以没有实例化之前会进入死循环
+    看做view的 get方法
+    */
+    
+//    override func loadView() {
+//        //print(view)
+//        
+//        //自定义视图
+//        if userLogin {
+//            
+//            //用户登录显示tableView视图
+//            super.loadView()
+//            return
+//        }
+//       
+//        //自定义访客视图
+//        let newView = UIView()
+//        newView.backgroundColor = UIColor.purpleColor()
+//        view = newView
+//    }
+    
+    //简化代码
+    override func loadView() {
+        //用户登录直接进入tableView表视图,没有登录就进入访客视图
+        userLogin ? super.loadView() :setVisitorView()
+    }
+    
+    private func setVisitorView() {
+        
+        //自定义访客视图
+        let newView = UIView()
+        newView.backgroundColor = UIColor.purpleColor()
+        view = newView
+
+    }
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
